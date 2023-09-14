065e396fbf22844b5ac92b0ea5a4b6251166e065
Romek <129655936+romekdomasik@users.noreply.github.com>
2023-09-14 11:33:03 +0200

    final

--- EVios4/View Controllers/AddExpensesViewController.swift
+++ EVios4/View Controllers/AddExpensesViewController.swift
@@ -38,17 +38,33 @@ class AddExpensesViewController: UIViewController, UIPickerViewDelegate, UIPicke
     }
     
     @IBAction func pressSaveExpenseBtn(_ sender: Any) {
-        guard let expenseName = expenseNameTF.text,
-              let valueText = expenseValueTF.text,
-              let value = Float(valueText)
-        else {return}
+        guard let expenseName = expenseNameTF.text, !expenseName.isEmpty else {
+            showAlert(title: "Champ manquant", message: "Le champ 'Nom de la dépense' ne peut pas être vide.")
+            return
+        }
+        
+        guard let valueText = expenseValueTF.text, !valueText.isEmpty else {
+            showAlert(title: "Champ manquant", message: "Le champ 'Valeur de la dépense' ne peut pas être vide.")
+            return
+        }
+        
+        guard let value = Float(valueText) else {
+            showAlert(title: "Format incorrect", message: "Le champ 'Valeur' doit contenir uniquement des nombres. Veuillez saisir un nombre valide.")
+            return
+        }
         
         let section = sections[expensePicker.selectedRow(inComponent: 0)]
         let selectedDate = datePicker.date
         
-        dismiss(animated: true){
+        dismiss(animated: true) {
             DataManager.shared.addExpense(name: expenseName, date: selectedDate, value: value, type: section)
         }
+    }
+
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
     }
     
     @IBAction func pressCancelBtn(_ sender: Any) {
